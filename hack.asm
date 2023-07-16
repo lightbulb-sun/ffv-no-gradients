9a9dab9ca2886f9e62746e941653ad108a9117ba
lightbulbsun <lightbulbsun.dev@gmail.com>
2023-07-16 10:56:28 +0200

    Change border colors from teal/gray to black

--- hack.asm
+++ hack.asm
@@ -8,4 +8,11 @@
         strb    r3, [r5]
         b       0x080ce8e0
 
+; change border color from teal to black
+.org 0x083ed9a8
+        .dh     0
+; change border color from gray to black
+.org 0x0836db48
+        .dh     0
+
 .close
